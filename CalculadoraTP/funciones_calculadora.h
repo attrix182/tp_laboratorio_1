8f589f7370e0233c6653a4da437ab244c7a75c18
Luciano S <44885834+attrix182@users.noreply.github.com>
2019-04-04 16:15:28 -0300

    Add files via upload

---
+++ CalculadoraTP/funciones_calculadora.h
@@ -0,0 +1,47 @@
+/** \brief Suma dos numeros
+ *
+ * \param float numero1 El primer sumando
+ * \param float numero2 El segundo sumando
+ * \return float La suma de los numeros ingresados
+ *
+ */
+float sumar(float numero1, float numero2);
+
+
+/** \brief Resta dos numeros
+ *
+ * \param float numero1 El minuendo
+ * \param float numero2 El sustraendo
+ * \return float Se obtiene la diferencia
+ *
+ */
+float restar(float numero1, float numero2);
+
+
+/** \brief Multiplica dos numeros
+ *
+ * \param float numero1 Primer factor
+ * \param float numero2 Segundo factor
+ * \return float Se obtiene la multiplicacion de ambos factores
+ *
+ */
+float multiplicar(float numero1, float numero2);
+
+
+/** \brief Divide dos numeros
+ *
+ * \param float numero1  Es el dividendo
+ * \param  float numero2  Es el divisor
+ * \return float Se obtiene el resultado de la division
+ * La funcion no valida si el divisor es 0
+ */
+float dividir(float numero1, float numero2);
+
+
+/** \brief Realiza el factorial de un numero
+ *
+ * \param double numero Numero del cual se quiere obtener su factorial
+ * \return double Se obtiene el factorial del numero
+ *
+ */
+double factorial(double numero);
---
+++ CalculadoraTP/menu.h
@@ -0,0 +1,14 @@
+/** \brief Muestra un menu de opciones
+ *
+ * \param valorActual1 float Ultimo valor de primer operando
+ * \param valorActual2 float Ultimo valor de segundo operando
+ * \return int Opcion seleccionada
+ *
+ */
+int pedirOpcionMenu(float valorActual1, float valorActual2);
+
+
+
+
+
+
